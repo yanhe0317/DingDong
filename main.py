921204dad2ab0bda3b3a74e46b7fddc155e6cba8
wanghaodong <2396017608@qq.com>
2022-04-21 04:57:58 +0800

    1

--- main.py
+++ main.py
@@ -27,7 +27,6 @@ if __name__ == "__main__":
             position = getPosition(image)
 
             if position:
-
                 showList.append((image, position))
 
         if len(showList) > 0:
@@ -38,15 +37,15 @@ if __name__ == "__main__":
             # 收获时间选择。
             elif "time.png" in [item[0] for item in showList]:
                 x, y = getPosition("time.png")
-                pyautogui.click(x, y, interval=0.5)
+                pyautogui.click(x, y, interval=0.1)
                 # 额外的时间段。（叮咚他妈天天更新，需要根据实际情况自己手动调整time.png和额外的时间段个数。）
                 for item in range(1):
                     if getPosition("time.png"):
-                        pyautogui.click(x, y=y-55, interval=0.5)
+                        pyautogui.click(x, y=y-55, interval=0.1)
             # 选择最优先的图片点击。
             else:
                 indexList = [item for item in showList if len(item[0].split("-")) > 1]
                 index = len(indexList)-1
                 if index >= 0:
                     x, y = indexList[index][1]
-                    pyautogui.click(x, y, interval=0.5)
+                    pyautogui.click(x, y, interval=0.1)
